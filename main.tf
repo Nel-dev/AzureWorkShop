f442b7e5db9c4ad73fe6d09c694db548a6638db5
Neldirson Castañeda <neldirson12@hotmail.com>
2023-08-11 21:01:35 -0500

    update

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_service_plan" "backend" {
   sku_name                 =  "P1v2"
 }
 
-resource "azurerm_Linux_web_app" "backend" {
+resource "azurerm_linux_web_app" "backend" {
   name                = "Backend-webapp-6666"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_service_plan.backend.location
